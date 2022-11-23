63360b65834cdaf6b970cb41b363c919aa2a4d8e
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-23 16:00:14 -0700

    Set up functions to handle key input

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
-const {connect} = require("./client");
+const { connect } = require("./client");
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = () => {
+  process.stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
