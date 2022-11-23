fda7bcd210cf3fa38558f24700d6514c1434694b
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-23 15:34:25 -0700

    Created a client file to host the connection function and exported it

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // event handler for incoming data
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Sohaib");
+  });
+
+  
+  
+
+
+  return conn;
+};
+
+
+
+module.exports = { connect };
